f1b0efcd6f4b97cdb8c23ddf0899f344b536a38e
Mohd Raza Shaikh <mohdraza7058@gmail.com>
2024-02-27 20:22:00 +0530

    001 code updated

--- 001_reverse_arr.cpp
+++ 001_reverse_arr.cpp
@@ -1,40 +1,57 @@
 #include <iostream>
 using namespace std;
 
+void printArr(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << "  ";
+    }
+    cout << endl;
+    return;
+}
+
+void reverseArr(int arr[], int n)
+{
+    // It will reverse the array without recursion.
+    int temp;
+    for (int i = 0; i <= n / 2; i++)
+    {
+        temp = arr[n - 1 - i];
+        arr[n - 1 - i] = arr[i];
+        arr[i] = temp;
+    }
+    return;
+}
+
+void reverseArrRecursive(int arr[], int start, int end)
+{
+    // It will reverse the array using recursion.
+    int temp;
+    if (start > end)
+        return;
+
+    temp = arr[end];
+    arr[end] = arr[start];
+    arr[start] = temp;
+    reverseArrRecursive(arr, start + 1, end - 1);
+}
+
 int main()
 {
-    /// REVERSE THE ARRAY
-
     int arr[] = {1, 2, 3, 4, 5};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    /**
+
+    int temp;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    /*
      * sizeof(arr) will return "no of elements in array x size of data-type of array"
      * sizeof(arr[0]) will return "size of data-type of first elemnt"
      */
 
-    int s = 0, e = size - 1, temp;
-    /*
-     * "s" is used to travel from left to right in array.
-     * "e" is used to travel from right to left in array.
-     * "temp" is used to store an element temporarily.
+    // reverseArr(arr, n);
+    reverseArrRecursive(arr, 0, n - 1);
+    printArr(arr, n);
 
-    */
-
-    // Reversing an array:
-    while (s < e)
-    {
-        temp = arr[s];
-        arr[s] = arr[e];
-        arr[e] = temp;
-        s++;
-        e--;
-    }
-
-    // Printing reverse array:
-    for (int i = 0; i <= size; i++)
-    {
-        cout << arr[i] << "  ";
-    }
     return 0;
 }
 
